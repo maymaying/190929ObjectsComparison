e374cb38232fde16be243b11ed6c7efe5ee0db6e
maymaying <1900750175@qq.com>
2019-10-04 08:49:44 +0800

    190929ObjectsComparison-对象的比较

---
+++ 190929--Java对象的比较/src/Student.java
@@ -0,0 +1,16 @@
+public class Student {
+    /**
+     * 学号
+     */
+    public String sn;
+
+    /**
+     * 姓名
+     */
+    public String name;
+
+    /**
+     * 年龄
+     */
+    public int age;
+}
